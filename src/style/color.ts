a58c2a7a7e97fa76a29c07f374b9e77349168b3a
Arman <arman_m121@yahoo.com>
2022-11-14 10:04:59 +0330

    add color

---
+++ src/style/color.ts
@@ -0,0 +1,19 @@
+export const PRIMARY = '#e8eaf6';
+export const PRIMARY_LIGHT = '#ffffff';
+export const PRIMARY_DARK = '#b6b8c3';
+export const SECONDARY = '#f96509';
+export const SECONDARY_LIGHT = '#ff9642';
+export const SECONDARY_DARK = '#be3300';
+export const TEXT_ON_PRIMARY = '##000000';
+export const TEXT_ON_SECONDARY = '##000000';
+
+export const GARY_1 = '#333333';
+export const GARY_2 = '#4F4F4F';
+export const GARY_3 = '#828282';
+export const GARY_4 = '#AEAFB4';
+export const GARY_5 = '#DEE0E5';
+
+export const INFO = '#3583EB';
+export const SUCCESS = '#1CA24A';
+export const WARNING = '#FFBD0A';
+export const ERROR = '#BF1A1A';
