1a698700c7b5a86ac853001ffd47174c18bba34e
Arman <arman_m121@yahoo.com>
2022-11-14 20:13:34 +0330

    create SplashScreen

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,38 @@
+import { View, Image, StyleSheet, StatusBar } from 'react-native'
+import React, { useEffect } from 'react'
+import image from '~/assets/image';
+import { Colors } from '~/style';
+
+export default function SplashScreen() {
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            // navigation.navigate('HomeStack')
+        }, 2000);
+    }, []);
+
+    return (
+        <>
+            <StatusBar backgroundColor={Colors.GARY_2} />
+
+            <View style={styles.container}>
+                <Image source={image.splash} style={styles.splash} />
+            </View>
+        </>
+    )
+}
+const styles = StyleSheet.create({
+    statusBar: {
+
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.GARY_2
+    },
+    splash: {
+        width: 250,
+        height: 250
+    }
+})
