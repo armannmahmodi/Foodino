b1b548750b504788445452215ef682ef60fa7ffd
Arman <arman_m121@yahoo.com>
2022-11-14 10:56:16 +0330

    add Spacing

---
+++ src/style/Spacing.ts
@@ -0,0 +1,20 @@
+import {scale, verticalScale} from '~/utils/Style';
+export const hairline = scale(5);
+export const tiny = scale(10);
+export const smallest = scale(15);
+export const smaller = scale(20);
+export const small = scale(24);
+export const base = scale(30);
+export const large = scale(35);
+export const larger = scale(40);
+export const largest = scale(45);
+export const extraLarge = scale(50);
+
+export const HEADER_HEIGHT = verticalScale(300);
+export const TAB_BAR_HEIGHT = verticalScale(75);
+export const DESCRIPTION_TOP = verticalScale(50);
+export const DESCRIPTION_PADDING = verticalScale(85);
+export const OTHER_TOP = verticalScale(50);
+export const OTHER_PADDING = verticalScale(7);
+export const TABS_HEIGHT = verticalScale(35);
+export const TABS_TOP = verticalScale(15);
---
+++ src/style/index.ts
@@ -0,0 +1,4 @@
+import * as Colors from '~/style/Color';
+import * as Spacing from '~/style/Spacing';
+
+export {Spacing, Colors};
