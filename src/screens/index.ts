182161c98a439c9fced67f88c5bb14ea7ad2c6b6
Arman <arman_m121@yahoo.com>
2022-11-15 20:22:06 +0330

    implement OrdersScreen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,7 @@
 export {default as SplashScreen} from '~/screens/SplashScreen';
 export {default as HomeScreen} from '~/screens/HomeScreen';
+export {default as CartScreen} from '~/screens/CartScreen';
+export {default as LoginScreen} from '~/screens/LoginScreen';
+export {default as OrdersScreen} from '~/screens/OrdersScreen';
 
 
