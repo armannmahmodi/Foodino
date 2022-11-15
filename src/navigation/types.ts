7f9aafd9e3286b2602d3103f378896e784fc6e24
Arman <arman_m121@yahoo.com>
2022-11-15 12:40:02 +0330

    implement navigation type

---
+++ src/navigation/types.ts
@@ -0,0 +1,19 @@
+
+import {MainStackParamList} from './MainStack';
+import {TabNavigatorStackParamList} from './TabNavigator';
+export type RootStackParamList = 
+MainStackParamList&
+  TabNavigatorStackParamList
+
+
+  declare global {
+    // Specifying this type is important if you heavily use useNavigation, Link etc. in your app since it'll ensure type-safety.
+    // READ MORE: https://reactnavigation.org/docs/typescript/#specifying-default-types-for-usenavigation-link-ref-etc
+  
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace ReactNavigation {
+      // eslint-disable-next-line @typescript-eslint/no-empty-interface
+      interface RootParamList extends RootStackParamList {}
+    }
+  }
+
