467d5cf2a64882b013f71ce8ef0eaac18e44cfb5
Arman <arman_m121@yahoo.com>
2022-11-17 13:51:02 +0330

    improve HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
-
-export default function Home() {
+import { HomeCard, AppBar } from '~/component';
+import { Colors } from '~/style'
+import { navigate } from '~/navigation/Methods';
+const renderItem = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <HomeCard />
     )
 }
+const item = [{ id: 1 }, { id: 1 }, { id: 1 }, { id: 1 }, { id: 1 }, { id: 1 }, { id: 1 }, { id: 1 }]
+export default function Home() {
+    return (
+
+        <View style={styles.container} >
+            <AppBar title='صفحه اصلی' icon='home' />
+            <View style={styles.flatList}>
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    // columnWrapperStyle={{ flexDirection: 'row-reverse' }}
+                    contentContainerStyle={{
+                        alignSelf: 'center',
+                    }}
+                    // numColumns={2}
+                    data={item}
+                    keyExtractor={(_, index) => `itm${index}`}
+                    renderItem={renderItem}
+                />
+            </View>
+        </View>
+
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.PRIMARY_LIGHT
+    },
+    flatList: {
+        paddingBottom: 70,
+        paddingTop: 10
+    }
+});
